f66a070ef5badff427f90efc3abf0503448bb2fc
Kostia S <kosiak5587@gmail.com>
2024-10-21 21:12:06 +0600

    feat:  сhange the default port

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import startServer from "./startServer";
-const { PORT } = process.env || 4000;
+const { PORT } = process.env || 5000; // .evn file PORT=4000
 
 startServer().listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
